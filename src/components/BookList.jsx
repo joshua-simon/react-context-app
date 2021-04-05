21a70a2abb3f387ea520596bd435ded525b480fe
Joshua-Simon <joshy.d.simon@gmail.com>
2021-04-05 13:23:35 +1200

    first commit

---
+++ src/components/BookList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BookList = () => {
+    return(
+        <div className = 'book-list'>
+            <ul>
+                <li>The way of kings</li>
+                <li>the name of the wind</li>
+                <li>the final empire</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList
