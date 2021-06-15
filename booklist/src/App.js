57ee8d1ab5d10df40e02ab22a9471d0ec3d4bda8
Joshua-Simon <joshy.d.simon@gmail.com>
2021-06-15 22:28:25 +1200

    created new booklist app

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import BookContextProvider from './contexts/BookContext'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return(
     <div>
       <BookContextProvider>
-        
+        <NavBar/>
       </BookContextProvider>
     </div>
   )
