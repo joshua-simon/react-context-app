0d937eeb3c8f58f866178de3c134777b5038ce06
Joshua-Simon <joshy.d.simon@gmail.com>
2021-06-17 11:07:27 +1200

    add function working

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import BookContextProvider from './contexts/BookContext'
 import NavBar from './components/NavBar'
 import BookList from './components/BookList'
+import BookForm from './components/BookForm'
 
 const App = () => {
   return(
@@ -9,6 +10,7 @@ const App = () => {
       <BookContextProvider>
         <NavBar/>
         <BookList/>
+        <BookForm/>
       </BookContextProvider>
     </div>
   )
