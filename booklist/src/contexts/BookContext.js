8ff1010a919e3fcfa6026683246ba27c9795ec1e
Joshua-Simon <joshy.d.simon@gmail.com>
2021-06-17 16:41:23 +1200

    reducers added

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,24 +1,14 @@
-import React, { createContext, useState } from 'react'
-import uuid from 'uuid/v1'
+import React, { createContext, useReducer } from 'react'
+import { BookReducer } from '../reducers/BookReducer'
+
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-        {title: 'the final empire', author: 'brandon sanderson', id: 2}
-    ])
+    const [books, dispatch] = useReducer(BookReducer, [])
 
-    const addBook = (title, author) => {
-        setBooks([...books, {title: title, author: author, id: uuid()}])
-    }
-    const removeBook = (id) => {
-        setBooks(books.filter(book => {
-            return book.id !== id
-        }))
-    }
     return (
-        <BookContext.Provider value = {{books, addBook, removeBook }}>
+        <BookContext.Provider value = {{books, dispatch }}>
             { props.children }
         </BookContext.Provider>
     )
